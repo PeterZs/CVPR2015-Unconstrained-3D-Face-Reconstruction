0133ad35eba5de7ff4633820401b7cf9726e6644
pole <heks93@163.com>
2016-01-18 17:13:37 +0800

    debug calculate L

--- warpTemplate.py
+++ warpTemplate.py
@@ -5,6 +5,7 @@ Created on Wed Jan 06 21:09:02 2016
 @author: 开圣
 """
 import numpy as np
+
 #calculate cot for L
 def calCot(vec1, vec2):
     cos = vec1.dot(vec2)/(np.sqrt(vec1.dot(vec1))*np.sqrt(vec2.dot(vec2)))
@@ -17,12 +18,20 @@ def calL(obj):
     ptsNum = len(obj.v)
     L = np.zeros((3*ptsNum, 3*ptsNum))
     for face in obj.face:
-        [p1, p2, p3] = map(np.array, map(lambda x:obj.v[x], face))
+        p = map(np.array, map(lambda x:obj.v[x], face))
         for i in range(3):
             for j in range(3):
-                if L[3*face[i], 3*face[j]] == 0:
-                    L[3*face[i], 3*face[j]] = calCot(p3-p1, p3-p2)
-                else:
-                    L[3*face[i], 3*face[j]] = 0.5 * (calCot(p3-p1, p3-p2) + L[3*face[i], 3*face[j]])    
+                if i != j:
+                    k = 3 - i -j
+                    if L[3*face[i], 3*face[j]] == 0:
+                        val = calCot(p[k]-p[i], p[k]-p[j])
+                        L[3*face[i], 3*face[j]] = val
+                        L[3*face[i]+1, 3*face[j]+1] = val
+                        L[3*face[i]+2, 3*face[j]+2] = val
+                    else:
+                        val = 0.5 * (calCot(p[k]-p[i], p[k]-p[i]) + L[3*face[i], 3*face[j]])
+                        L[3*face[i], 3*face[j]] = val
+                        L[3*face[i]+1, 3*face[j]+1] = val
+                        L[3*face[i]+2, 3*face[j]+2] = val
     return L
         
