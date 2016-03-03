d9e925dc43aaabce17a30542ad2304858f9e9da0
pole <heks93@163.com>
2016-03-03 16:18:07 +0800

    continue the work

--- warpTemplate.py
+++ warpTemplate.py
@@ -5,7 +5,7 @@ Created on Wed Jan 06 21:09:02 2016
 @author: 开圣
 """
 import numpy as np
-
+from PIL import Image,ImageDraw
 #calculate cot for L
 def calCot(vec1, vec2):
     cos = vec1.dot(vec2)/(np.sqrt(vec1.dot(vec1))*np.sqrt(vec2.dot(vec2)))
@@ -34,7 +34,35 @@ def calL(obj):
                         L[3*face[i]+1, 3*face[j]+1] = val
                         L[3*face[i]+2, 3*face[j]+2] = val
     return L
+
+# calculate P (weakly perspective)    
+def calP(landmark2D, landmark3D):
+    b1 = landmark2D[:,0]
+    b2 = landmark2D[:,1]
+    A = np.c_[landmark3D, np.ones((len(landmark3D), 1))]
+    #import pdb
+    #pdb.set_trace()
+    m1 = np.linalg.lstsq(A, b1)[0]
+    m2 = np.linalg.lstsq(A, b2)[0]
+    M = np.c_[m1, m2, np.array([0,0,0,1])]
+    return M.T
+
     
-def calH(obj):
-     
-        
+def drawL(imgPath, landmark):
+    img = Image.open(imgPath)
+    imDraw = ImageDraw.Draw(img)
+    radius = 10
+    for p in landmark:
+        x = p[0]
+        y = p[1]
+        imDraw.ellipse((x - radius,y -radius,x + radius, y + radius), fill = 'red', outline = 'red')
+    return img
+
+#load landmark index    
+def loadLandmark(fp):
+    text = open(fp, 'r')    
+    landmarkIndex = []
+    for line in text:
+        line = line.split()[0]
+        landmarkIndex.append(int(float(line)))
+    return landmarkIndex
